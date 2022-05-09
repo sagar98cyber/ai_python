5dc8f22f53001ab5dc88ab45051bc2c230b7d03a
A HEAD FULL OF DREAMS <040@nfsu.ac.in>
2022-05-09 03:01:12 -0700

    Linear Regression

--- pracs 6/LinearRegression.py
+++ pracs 6/LinearRegression.py
@@ -1,19 +1,9 @@
-from cProfile import label
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-df = pd.read_csv('pracs 6\headbrain.csv')
+#%matplotlib inline
+train = pd.read_csv("train.csv")
+train.head()
 
-X = df['Head Size(cm^3)'].values
-Y = df['Brain Weight(grams)'].values
-print(df.shape)
-df.head()
-
-np.corrcoef(X,Y)
-plt.scatter(X,Y,c= 'Green',label = 'Data points')
-plt.xlabel('Head Size in cm3')
-plt.ylabel('Brain Weight in grams')
-plt.legend()
-plt.show()
-
+train.count()
